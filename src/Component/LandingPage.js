b27ad8abd1b411f23292c8aa492b1d885715f949
Jitendra Singh <jitendersingh100592@gmail.com>
2022-09-22 12:59:26 +0530

    bg added,landing chng, signup chn

--- src/Component/LandingPage.js
+++ src/Component/LandingPage.js
@@ -11,7 +11,7 @@ export default function LandingPage() {
   return (
     <>
         
-         <img src = "https://media-exp1.licdn.com/dms/image/C4D1BAQE39JMrep2Zhg/company-background_10000/0/1647846459365?e=2147483647&v=beta&t=zQuro9mJ1kHHKaHYKSA99NQe8EH6C3rNiDfOAO3d47Q" alt ="Into" style={myStyle} />
+         <img src = "./bg.jpg" alt ="Into" style={myStyle} />
          </>
   )
 }
